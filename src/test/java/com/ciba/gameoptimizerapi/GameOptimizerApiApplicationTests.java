5cb2ae20508d8ddd05203638a70c9c2654e2d20d
Costache Ioana-Brandusa-Alexandra <costachealexandra28@gmail.com>
2024-01-14 12:27:58 +0200

    Removed useless annotation that ruins the test

--- src/test/java/com/ciba/gameoptimizerapi/GameOptimizerApiApplicationTests.java
+++ src/test/java/com/ciba/gameoptimizerapi/GameOptimizerApiApplicationTests.java
@@ -2,10 +2,8 @@ package com.ciba.gameoptimizerapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootTest
-@EnableOpenApi
 class GameOptimizerApiApplicationTests {
 
 	@Test
