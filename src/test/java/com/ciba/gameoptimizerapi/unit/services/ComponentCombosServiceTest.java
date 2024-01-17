e235e4d5a2022b65cceb8a1bf71ef5c3c4b2a2af
Costache Ioana-Brandusa-Alexandra <costachealexandra28@gmail.com>
2024-01-17 08:06:37 +0200

    Added test for delete methods

--- src/test/java/com/ciba/gameoptimizerapi/unit/services/ComponentCombosServiceTest.java
+++ src/test/java/com/ciba/gameoptimizerapi/unit/services/ComponentCombosServiceTest.java
@@ -1,9 +1,12 @@
 package com.ciba.gameoptimizerapi.unit.services;
 
+import com.ciba.gameoptimizerapi.exceptions.BadRequestException;
 import com.ciba.gameoptimizerapi.models.Component;
+import com.ciba.gameoptimizerapi.models.ComponentsCombo;
 import com.ciba.gameoptimizerapi.models.jooq.enums.ComponentType;
 import com.ciba.gameoptimizerapi.repositories.Component.ComponentRepository;
-import com.ciba.gameoptimizerapi.services.Component.ComponentServiceImpl;
+import com.ciba.gameoptimizerapi.repositories.ComponentsCombo.ComponentsComboRepository;
+import com.ciba.gameoptimizerapi.services.ComponentCombos.ComponentCombosServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,62 +15,66 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-public class ComponentServiceTest {
+public class ComponentCombosServiceTest {
 
     @InjectMocks
-    private ComponentServiceImpl service;
+    private ComponentCombosServiceImpl service;
+
+//    @Mock
+//    private ComponentRepository componentRepository;
 
     @Mock
-    private ComponentRepository repository;
+    private ComponentsComboRepository repository;
 
     private Component processor;
     private Component graphics;
     private Component ram;
+    private ComponentsCombo combo;
 
-    private List<Component> allComponents;
+//    private List<Component> allComponents;
 
     @BeforeEach
     void init() {
         processor = Component.builder()
-                .id(UUID.randomUUID())
+                .id(UUID.fromString("34b8d0f5-90af-439f-9054-b6d982c27662"))
                 .name("Intel Core i5-6600")
                 .type(ComponentType.processor)
                 .capacity(3.3F)
                 .build();
 
         graphics = Component.builder()
-                .id(UUID.randomUUID())
+                .id(UUID.fromString("53606477-fa18-425f-8f25-cf630584f1da"))
                 .name("NVIDIA GeForce GTX 960")
                 .type(ComponentType.graphics_card)
                 .capacity(4.0F)
                 .build();
 
         ram = Component.builder()
-                .id(UUID.randomUUID())
+                .id(UUID.fromString("e6966a01-4c13-4786-a7d7-b96535b809ef"))
                 .type(ComponentType.ram)
                 .capacity(6.0F)
                 .build();
 
-        allComponents = List.of(processor, graphics, ram);
+        combo = ComponentsCombo.builder().id(UUID.randomUUID())
+                .ramId(ram.getId())
+                .graphcisCardId(graphics.getId())
+                .processorId(processor.getId())
+                .build();
+
+//        allComponents = List.of(processor, graphics, ram);
     }
 
     @Test
-    void getALlComponents_shouldSucceed() {
-        given(repository.getAllComponents()).willReturn(allComponents);
-
-        assertFalse(service.getAllComponents().isEmpty());
-    }
-
-    @Test
-    void deleteComponent_shouldSucceed() {
-        service.delete(processor.getId());
-        verify(repository).deleteByUUID(processor.getId());
+    void delete_shouldSucceed() {
+        service.delete(combo.getId());
+        verify(repository).deleteByUUID(combo.getId());
     }
 }
